25fca91f992f3b16e6a879d0c2d8c4f862f258d7
Jimmy Hough Jr <jimmyhoughjr@me.com>
2023-07-10 13:29:11 -0500

    rep attempt

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,26 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+
+      func testGetExistingTodo() async throws {
+        let app = Application(.testing)
+        do {
+            defer { app.shutdown() }
+            try await configure(app)
+            try await app.autoMigrate()
+            
+            let todo = Todo(title: UUID().uuidString)
+            try await todo.create(on: app.db)
+            
+            try app.test(.GET, "todos") { res in
+                XCTAssertEqual(res.status, .ok)
+                XCTAssertContent(Array<Todo>.self, res) { content in
+                    XCTAssert(content.contains { $0.id == todo.id! })
+                }
+            }
+        } catch {
+            app.logger.report(error: error)
+            throw error
+        }
+    }
 }
